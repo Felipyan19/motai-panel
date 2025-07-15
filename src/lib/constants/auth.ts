81be47c8dcbe773bc20fa61349c95ba07477abde
Felipyan19 <felipyan199@gmail.com>
2025-07-14 21:46:35 -0500

    feat: add authentication constants, schemas, and token utility functions. Ac

---
+++ src/lib/constants/auth.ts
@@ -0,0 +1,11 @@
+export const TOKEN = "motai-token";
+export const MOCK_TOKEN = "fddfffsasadsaaaAAASDA";
+export const MOCK_CREDENTIALS = {
+  username: "admin@motai.app",
+  password: "admin123",
+};
+export const COOKIE_OPTIONS = {
+  httpOnly: true,
+  maxAge: 60 * 60 * 24 * 30,
+  path: "/",
+};
---
+++ src/lib/utils/token.ts
@@ -0,0 +1,12 @@
+import { cookies } from "next/headers";
+import { TOKEN, COOKIE_OPTIONS } from "@/lib/constants/auth";
+
+export const setToken = async (token: string) => {
+    const cookieStore = await cookies();
+    cookieStore.set(TOKEN, token, COOKIE_OPTIONS);
+  };
+
+export const getToken = async () => {
+  const cookieStore = await cookies();
+  return cookieStore.get(TOKEN)?.value;
+};
